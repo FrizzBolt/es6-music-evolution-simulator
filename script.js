979a5d4dff5c89cd21e000e73772b39f2d0a5fb1
Frizzbolt <joshua.a.abrams@gmail.com>
2016-06-22 09:59:33 -0500

    Prepare MelodyMutator implementation

---
+++ script.js
@@ -0,0 +1,25 @@
+const NOTES_IN_SCALE = 8;
+const DURATION_OF_MELODY = 12;
+const BEATS_PER_MINUTE = 120;
+const MAXIMUM_NOTE_LENGTH = 4;
+const MUTATION_FREQUENCY = 1;
+
+var currentMelody;
+
+$("#generate").click(function(){
+    var tone = new Tone(0)
+    var scale = new Scale(tone);
+    var generator = new RandomMelodyGenerator(scale);
+    var randomMelody = generator.generateRandomMelody();
+    currentMelody = randomMelody
+    var visualizer = new MelodyVisualizationConverter(randomMelody);
+    visualizer.convertMelodyToArray()
+});
+
+$("#play").click(function(){
+		Player.play(currentMelody)
+})
+
+$("#mutate").click(function(){
+		var durationSubMutator = new DurationSubMutator
+})
